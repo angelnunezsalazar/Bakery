2838360808ed55c5827c495e14ec5ca24ce0101c
Snahider <snahider@gmail.com>
2012-06-08 02:11:45 -0500

    Online events without sync request

--- Bakery.Web/Scripts/App/Data/connection.js
+++ Bakery.Web/Scripts/App/Data/connection.js
@@ -2,31 +2,15 @@
     var onLine = true,
         lastOnLineStatus = true;
 
-    function testConnection() {
+    (function () {
+        initConnection();
+        setInterval(function () {
+            updateConnection();
+        }, 5000);
+    })();
+
+    function initConnection() {
         var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState != 4) {
-                return;
-            }
-
-            if (xhr.status == 200) {
-                fireEvent("goodconnection", {});
-                clearTimeout(noResponseTimer);
-                // Save the data to local storage
-                localStorage[url] = xhr.responseText;
-                // call the handler
-                callback(xhr.responseText);
-            } else {
-                fireEvent("connectionerror", {});
-                // There is an error of some kind, use our cached copy (if available).
-                if (!!localStorage[url]) {
-                    // We have some data cached, return that to the callback.
-                    callback(localStorage[url]);
-                    return;
-                }
-            }
-        };
-
         xhr.open('HEAD', '/', false);
         try {
             xhr.send();
@@ -34,7 +18,30 @@
         } catch (e) {
             onLine = false;
         }
-        return onLine;
+
+        lastOnLineStatus = onLine;
+    }
+
+    function updateConnection() {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState != 4) {
+                return;
+            }
+            if (xhr.status == 200) {
+                onLine = true;
+            } else {
+                onLine = false;
+            }
+
+            if (onLine !== lastOnLineStatus) {
+                triggerEvent(onLine ? 'online' : 'offline');
+                lastOnLineStatus = onLine;
+            }
+        };
+
+        xhr.open('HEAD', '/');
+        xhr.send();
     }
 
     function triggerEvent(type) {
@@ -43,20 +50,4 @@
         event.eventName = type;
         (document.body || window).dispatchEvent(event);
     }
-
-//    (function() {
-//        testConnection();
-//        if (onLine === false) {
-//            lastOnLineStatus = false;
-//            triggerEvent('offline');
-//        }
-//        setInterval(function() {
-//            testConnection();
-//            if (onLine !== lastOnLineStatus) {
-//                triggerEvent(onLine ? 'online' : 'offline');
-//                lastOnLineStatus = onLine;
-//            }
-//        }, 5000);
-//    })();
-
 } ());
